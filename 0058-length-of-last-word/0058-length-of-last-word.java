799aaa1a24fb975f86191868b5f871941a4d3ce6
Vinothini9032 <vinoanbu2005@gmail.com>
2025-10-07 11:55:45 +0530

    Delete 0058-length-of-last-word/0058-length-of-last-word.java

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        s=s.trim();
-        int lastspace=s.lastIndexOf(' ');
-        if(lastspace==-1)return s.length();
-        return s.length()-lastspace-1;
-        
-    }
-}
